590fd69451af26fcbf13cdafa4226af3d7163ab5
SimonGrchevski <simongrcevski@gmail.com>
2021-09-30 16:22:36 +0200

    Add tables invoice_items, invoice and mid

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,4 @@
--- Create a new database called 'patients'
+-- Create a new TABLE called 'patients'
 CREATE TABLE patients (
     id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
@@ -6,8 +6,8 @@ CREATE TABLE patients (
     PRIMARY KEY(id)
 );
 
--- Create a new database called 'medical_histories'
-CREATE DATABASE medical_histories (
+-- Create a new TABLE called 'medical_histories'
+CREATE TABLE medical_histories (
     id INT GENERATED ALWAYS AS IDENTITY,
     admitted_at TIMESTAMP,
     patient_id INT,
@@ -16,10 +16,40 @@ CREATE DATABASE medical_histories (
     PRIMARY KEY(id)
 );
 
--- Create a new database called 'treatments'
-CREATE DATABASE treatments (
+-- Create a new TABLE called 'treatments'
+CREATE TABLE treatments (
     id INT GENERATED ALWAYS AS IDENTITY,
     type varchar(100),
     name varchar(100),
     PRIMARY KEY(id)
+);
+
+-- Create a new TABLE called 'invoices'
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    PRIMARY KEY(id)
+);
+-- Create a new TABLE called 'invoice_items'
+CREATE TABLE invoice_items (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT,
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+    PRIMARY KEY(id)
+);
+-- Create a new TABLE called 'mid'
+CREATE TABLE mid (
+    medical_history_id INT,
+    treatment_id INT,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
